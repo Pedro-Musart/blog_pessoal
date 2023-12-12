b6813e09bd87943eae1e11e716574f2ac9c7660c
Pedro Soares <pedrowesleyfs@gmail.com>
2023-12-12 15:40:20 -0300

    criado o CRUD da tb_tema

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,10 +1,15 @@
 package com.generation.blogpessoal.model;
 import java.time.LocalDateTime;
+
 import org. hibernate. annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta. persistence.GeneratedValue;
 import jakarta. persistence. GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence. Table;
 import jakarta. validation. constraints. NotBlank;
 import jakarta.validation.constraints.Size;
@@ -28,6 +33,10 @@ private String texto;
 @UpdateTimestamp
 private LocalDateTime data;
 
+
+@ManyToOne
+@JsonIgnoreProperties("postagem")
+private Tema tema;
 
 public Long getId() {
 	return id;
@@ -60,5 +69,15 @@ public LocalDateTime getData() {
 public void setData(LocalDateTime data) {
 	this.data = data;
 }
+
+public Tema getTema() {
+	return tema;
+}
+
+public void setTema(Tema tema) {
+	this.tema = tema;
+}
+
+
 
 }
