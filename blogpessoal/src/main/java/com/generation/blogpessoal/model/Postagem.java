09f3c5e23e6e09cb00576d55069d7252bdbe2750
Pedro Soares <pedrowesleyfs@gmail.com>
2023-12-20 21:39:29 -0300

    criado a classe usuário e o security da aplicação

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,83 +1,93 @@
 package com.generation.blogpessoal.model;
+
 import java.time.LocalDateTime;
 
-import org. hibernate. annotations.UpdateTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
-import jakarta. persistence.GeneratedValue;
-import jakarta. persistence. GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence. Table;
-import jakarta. validation. constraints. NotBlank;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
+    public Long getId() {
+        return this.id;
+    }
 
-@NotBlank (message = "O atributo título é Obrigatório!")
-@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-private String titulo;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-@NotBlank(message = "0 atributo texto é Obrigatório!")
-@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
-private String texto;
+    public String getTitulo() {
+        return this.titulo;
+    }
 
-@UpdateTimestamp
-private LocalDateTime data;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
+    public String getTexto() {
+        return this.texto;
+    }
 
-@ManyToOne
-@JsonIgnoreProperties("postagem")
-private Tema tema;
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 
-public Long getId() {
-	return id;
-}
+    public LocalDateTime getData() {
+        return this.data;
+    }
 
-public void setId(Long id) {
-	this.id = id;
-}
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 
-public String getTitulo() {
-	return titulo;
-}
+	public Tema getTema() {
+		return tema;
+	}
 
-public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
-public String getTexto() {
-	return texto;
-}
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
-public void setTexto(String texto) {
-	this.texto = texto;
-}
-
-public LocalDateTime getData() {
-	return data;
-}
-
-public void setData(LocalDateTime data) {
-	this.data = data;
-}
-
-public Tema getTema() {
-	return tema;
-}
-
-public void setTema(Tema tema) {
-	this.tema = tema;
-}
-
-
-
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+    
 }
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.query.Param;
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-public List < Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
